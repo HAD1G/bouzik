360e443499dd194d08ad979dc948800c21c61172
HAD1G <hadialjawad237@gmail.com>
2024-11-12 21:00:48 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #171717;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+}
+
+.game-container {
+    position: relative;
+    width: 400px;
+    height: 600px;
+    background-color: #171717;
+}
+
+.table {
+    background-color: #c10000;
+    border-radius: 10px;
+    width: 250px;
+    height: 350px;
+    margin: 0 auto;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.title {
+    font-size: 36px;
+    font-family: "Brush Script MT", cursive;
+    color: #fff;
+}
+
+.played-cards .card {
+    font-size: 24px;
+    background-color:
